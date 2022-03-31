62ad533f2bed3bf1400d187420123212451de85e
AA Student <appacademy@Mini-32.local>
2022-03-30 17:35:41 -0700

    Edits artwork

--- FirstRoute/app/models/user.rb
+++ FirstRoute/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-
-    
-
+    has_many :artworks,
+        primary_key: :id,
+        foreign_key: :artist_id,
+        class_name: :Artwork
 end
---
+++ FirstRoute/db/migrate/20220331001343_change_user.rb
@@ -0,0 +1,7 @@
+class ChangeUser < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :email
+    rename_column :users, :name, :username
+  end
+  add_index :users, :name, unique: true
+end
